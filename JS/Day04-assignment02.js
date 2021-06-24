661e860e60df8f3b0401ee9ba13eb07f25eaa592
Carlo Tac-an <carlo.tacan1@gmail.com>
2021-06-24 23:46:28 +0800

    Created a file for Day 04 assignment 02

---
+++ JS/Day04-assignment02.js
@@ -0,0 +1,13 @@
+// Day04 assignment 02:
+// 1. Create application that plays Rock-Paper-Scissors indefinitely (using while loop)
+// 2. It should have a difficulty level based on user's winrate (e.g., if 90% winrate, 90% of the time, the user should win the game; involves manipulation of rng)
+// 3. The user should be able to exit whenever he/she wants.
+// 
+// Flow: 
+// Ask the user first his/her action
+// Computer's action
+// Record winner
+// Ask if he/she wants to continue or exit
+// If exit, it should show the results
+// 
+// Needs knowledge of inputting into JS
