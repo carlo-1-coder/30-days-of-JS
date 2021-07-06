dcf18eaec483acf17788c31eaf9196317cb14f0d
carlo-1-coder <carlo.tacan1@gmail.com>
2021-07-06 18:39:32 +0800

    S004-06 unfinished

---
+++ JS/StackLeague/S005-210701-AddingMatrices.js
@@ -0,0 +1,14 @@
+// SILVER 005: Adding Matrices; Submitted: Jul 01, 2021 09:46 AM; 100/100
+
+// Adding matrices
+// Write a function that accepts two square matrices (N x N two dimensional arrays), and return the sum of the two. Both matrices being passed into the function will be of size N x N (square), containing only integers.
+// How to add matrices?
+// HINT
+// https://stattrek.com/matrix-algebra/matrix-addition.aspx
+// NOTE:
+// check the sample test for more basis
+// Examples:
+
+
+
+// From <https://stackleague.com/dashboard/challenge/4lhRsVfRC5Q6usgAOd3pBIquKlQmreWaEnfV6XXrEAM> 
